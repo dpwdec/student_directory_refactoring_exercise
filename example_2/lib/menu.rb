b36f98a5c1aa6cf3b218936e428ddf6bf028e610
dpwdec <declankolakowski@gmail.com>
2020-04-22 10:48:06 +0100

    Added print menu test@

--- example_2/lib/menu.rb
+++ example_2/lib/menu.rb
@@ -1,5 +1,10 @@
 class Menu
   def print_menu
     puts "1. Input the students"
+    puts "2. Show the students"
+    puts "3. Save the list to a csv file"
+    puts "4. Load the list from a csv file"
+    puts "5. Print the source code for this programme"
+    puts "9. Exit"
   end
 end
--- example_2/spec/menu_spec.rb
+++ example_2/spec/menu_spec.rb
@@ -4,7 +4,7 @@ describe Menu do
 
   describe '#print_menu' do
     it 'outputs the menu' do
-      expect { subject.print_menu }.to output("1. Input the students\n").to_stdout
+      expect { subject.print_menu }.to output("1. Input the students\n2. Show the students\n3. Save the list to a csv file\n4. Load the list from a csv file\n5. Print the source code for this programme\n9. Exit\n").to_stdout
     end
   end
 
